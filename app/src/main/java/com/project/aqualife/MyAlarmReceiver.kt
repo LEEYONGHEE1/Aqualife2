e28d4d013dae78a6ca2da8874c5715dbc0669ab7
LEEYONGHEE1 <lyh990104@naver.com>
2023-02-21 23:40:49 +0900

    receiver error

--- app/src/main/java/com/project/aqualife/MyAlarmReceiver.kt
+++ app/src/main/java/com/project/aqualife/MyAlarmReceiver.kt
@@ -4,13 +4,21 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.lifecycle.ViewModelProvider
+import com.project.aqualife.viewModel.AuthViewModel
 
 class MyAlarmReceiver : BroadcastReceiver() {
+
+    private lateinit var authViewModel : AuthViewModel
 
     override fun onReceive(context: Context, intent: Intent) {
         if (intent?.action == "MyBroadcastReceiverAction") {
             Log.d("알람","RECEIVED")
 
+            authViewModel = ViewModelProvider(requireActivity())[AuthViewModel::class.java]
+
+
+            authViewModel.changeFiltSetting((activity as MainActivity).getSpinnerData().first, daycode, filtTime, chargeCount)
 
         }
     }
