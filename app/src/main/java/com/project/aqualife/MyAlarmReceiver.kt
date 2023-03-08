711636d903765688c2d3b72a05456b1f340e63a2
LEEYONGHEE1 <lyh990104@naver.com>
2023-03-08 23:31:15 +0900

    receiver error

--- app/src/main/java/com/project/aqualife/MyAlarmReceiver.kt
+++ app/src/main/java/com/project/aqualife/MyAlarmReceiver.kt
@@ -22,4 +22,14 @@ class MyAlarmReceiver : BroadcastReceiver() {
         }
     }
 
+    private fun cancelAlarm() {
+        val alarmManager = context?.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val intent = Intent(context, MyAlarmReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0)
+        // cancel the alarm
+        alarmManager.cancel(pendingIntent);
+        // delete the PendingIntent
+        pendingIntent.cancel()
+    }
+
 }
